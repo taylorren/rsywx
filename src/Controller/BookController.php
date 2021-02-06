7fb0d43c3f12b38288867bed4c109f22d918e280
Taylor Ren <taylor.ren@gmail.com>
2021-02-06 16:53:02 +0800

    A lot of changes.

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,38 +4,43 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-
 use App\Service\GuzzleService;
 
-class BookController extends AbstractController
-{
+class BookController extends AbstractController {
+
     private $service;
-    
-    public function __construct(GuzzleService $service)
-    {
-        $this->service=$service;
+
+    public function __construct(GuzzleService $service) {
+        $this->service = $service;
     }
-    public function detail($bookid): Response
-    {
-        $uri="book/$bookid";
-        $book= json_decode((string)($this->service->getService()->get($uri)->getBody()))->data;
+
+    public function detail($bookid): Response {
+        $uri = "book/$bookid";
+        $res = $this->service->getService()->get($uri);
         
-        return $this->render('book/detail.html.twig', ['book'=>$book]);
-    }
-    
-    public function latestBook($max=1):Response
-    {
-        $uri="book/latest/$max";
-        $res= json_decode((string)($this->service->getService()->get($uri)->getBody()))->data[0];
+        if($res->getStatusCode()== 404)//book not found
+        {
+            //TODO: redirec to a better looking "not found page"
+            throw $this->createNotFoundException("Not found");
+        }
         
-        return $this->render('widget/default/latest_book.html.twig', ['res'=>$res]);
-    }
-    
-    public function randombook($max=3):Response
-    {
-        $uri="book/random/$max";
-        $res= json_decode((string)($this->service->getService()->get($uri)->getBody()))->data;
+        $book=json_decode((string)$res->getBody())->data;
         
-        return $this->render('widget/default/random_book.html.twig', ['res'=>$res]);
+        return $this->render('book/detail.html.twig', ['book' => $book]);
     }
+
+    public function latestBook($max = 1): Response {
+        $uri = "book/latest/$max";
+        $res = json_decode((string) ($this->service->getService()->get($uri)->getBody()))->data[0];
+
+        return $this->render('widget/default/latest_book.html.twig', ['res' => $res]);
+    }
+
+    public function randombook($max = 3): Response {
+        $uri = "book/random/$max";
+        $res = json_decode((string) ($this->service->getService()->get($uri)->getBody()))->data;
+
+        return $this->render('widget/default/random_book.html.twig', ['res' => $res]);
+    }
+
 }
--- src/Service/GuzzleService.php
+++ src/Service/GuzzleService.php
@@ -4,19 +4,30 @@ namespace App\Service;
 
 use GuzzleHttp\Client;
 
-class GuzzleService
-{
+class GuzzleService {
+
     private $service;
-    
-    public function __construct()
-    {
-        $this->service=new Client([
-            'base_uri' => 'https://api.rsywx.com/',
-        ]);
+
+    public function __construct() {
+        $env=$_ENV['APP_ENV'];
+        
+        if ($env == 'prod') {
+            $this->service = new Client([
+                'base_uri' => 'https://api.rsywx.com/',
+                'http_errors' =>false,
+            ]);
+        }
+        else
+        {
+            $this->service = new Client([
+                'base_uri' => 'http://api/',
+                'http_errors'=>false,
+            ]);
+        }
     }
-    
-    public function getService()
-    {
+
+    public function getService() {
         return $this->service;
     }
+
 }
