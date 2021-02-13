db25a8bd71587fac63d223a989bc5aa2a651d47b
Taylor Ren <taylor.ren@gmail.com>
2021-02-13 16:33:26 +0800

    Fixed a bug when new tag is empty.

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -16,37 +16,34 @@ class BookController extends AbstractController {
 
     public function detail($bookid): Response {
         $uri = "book/$bookid";
-        try
-        {
+        try {
             $res = $this->service->getService()->get($uri);
-        }
-        catch (\GuzzleHttp\Exception\ClientException $e)
-        {
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
             //TODO: redirec to a better looking "not found page"
-            echo('error');die();
+            echo('error');
+            die();
         }
-        
-        $book=json_decode((string)$res->getBody())->data;
-        
+
+        $book = json_decode((string) $res->getBody())->data;
+
         return $this->render('book/detail.html.twig', ['book' => $book]);
     }
 
     public function latestBook($max = 1): Response {
         $uri1 = "book/latest/$max"; //get the latest one book
-        $uri2='book'; //get summary 
-      
+        $uri2 = 'book'; //get summary 
         //$book=json_decode((string)$this->service->getService()->get($uri1)->getBody())->data[0];
         //$summary=json_decode($this->service->getService()->get($uri2)->getBody())->data;
-        
-        $promises=[
-            'book'=>$this->service->getService()->getAsync($uri1),
-            'summary'=>$this->service->getService()->getAsync($uri2),
+
+        $promises = [
+            'book' => $this->service->getService()->getAsync($uri1),
+            'summary' => $this->service->getService()->getAsync($uri2),
         ];
-        
-        $responses= \GuzzleHttp\Promise\Utils::unwrap($promises);
-        $book=json_decode((string)$responses['book']->getBody())->data[0];
-        $summary=json_decode((string)$responses['summary']->getBody())->data;
-        return $this->render('widget/default/latest_book.html.twig', ['book' => $book, 'summary'=>$summary]);
+
+        $responses = \GuzzleHttp\Promise\Utils::unwrap($promises);
+        $book = json_decode((string) $responses['book']->getBody())->data[0];
+        $summary = json_decode((string) $responses['summary']->getBody())->data;
+        return $this->render('widget/default/latest_book.html.twig', ['book' => $book, 'summary' => $summary]);
     }
 
     public function randomBook($max = 3): Response {
@@ -55,52 +52,49 @@ class BookController extends AbstractController {
 
         return $this->render('widget/default/random_book.html.twig', ['book' => $book[0]]);
     }
-    
-    public function list($type, $key, $page):Response
-    {
-        $uri="book/search/$type/$key/$page";
-        $res= json_decode((string) ($this->service->getService()->get($uri)->getBody()))->data;
-        
-        $search=[
-            'type'=>$type, 
-            'key'=>$key, 
-            'page'=>$page];
-        return $this->render('book/list.html.twig', ['res'=>$res, 'search'=>$search]);
-    }
-    
-    public function search(\Symfony\Component\HttpFoundation\Request $req)
-    {
-        $key=$req->get("keyword");
-        return $this->redirect($this->generateUrl("list", ['type'=>'various', 'key'=>$key, 'page'=>1]));
-    }
-    
-    public function addTag(\Symfony\Component\HttpFoundation\Request $req)
-    {
-        $newtags=trim($req->get('newtags'));
-        $id=$req->get('id');
-        $bookid=$req->get('bookid');
-        
-        $uri="book/add_tag/$id/$newtags";
-        
-        
-        //$data= json_encode(['tags'=>$newtags, 'id'=>$id]);
 
-        $res=$this->service->getService()->get($uri);
-        
-        $uri=$this->generateUrl('detail', ['bookid'=>$bookid]);
-        
-        return $this->redirect($uri);
+    public function list($type, $key, $page): Response {
+        $uri = "book/search/$type/$key/$page";
+        $res = json_decode((string) ($this->service->getService()->get($uri)->getBody()))->data;
+
+        $search = [
+            'type' => $type,
+            'key' => $key,
+            'page' => $page];
+        return $this->render('book/list.html.twig', ['res' => $res, 'search' => $search]);
     }
-    
-    public function today()
-    {
-        $today=new \DateTime();
-        $m=$today->format('m');
-        $d=$today->format("d");
-        $uri="book/today/$m/$d";
-        
-        $res=json_decode((string)($this->service->getService()->get($uri)->getBody()))->data;
-        
-        return $this->render("book/today.html.twig", ['books'=>$res->books, 'm'=>$m, 'd'=>$d]);
+
+    public function search(\Symfony\Component\HttpFoundation\Request $req) {
+        $key = $req->get("keyword");
+        return $this->redirect($this->generateUrl("list", ['type' => 'various', 'key' => $key, 'page' => 1]));
     }
+
+    public function addTag(\Symfony\Component\HttpFoundation\Request $req) {
+
+        $newtags = trim($req->get('newtags'));
+
+        $id = $req->get('id');
+        $bookid = $req->get('bookid');
+        $uri = $this->generateUrl('detail', ['bookid' => $bookid]);
+        if ($newtags != '') {
+            $action_uri = "book/add_tag/$id/$newtags";
+            $this->service->getService()->get($action_uri);
+            
+            return $this->redirect($uri);
+        } else {
+            return $this->redirect($uri);
+        }
+    }
+
+    public function today() {
+        $today = new \DateTime();
+        $m = $today->format('m');
+        $d = $today->format("d");
+        $uri = "book/today/$m/$d";
+
+        $res = json_decode((string) ($this->service->getService()->get($uri)->getBody()))->data;
+
+        return $this->render("book/today.html.twig", ['books' => $res->books, 'm' => $m, 'd' => $d]);
+    }
+
 }
